5c3e904c3836188afb5cdaf077ac1fc2aeaf54e6
Osi2 <Yegor.Osipov@gmail.com>
2015-06-28 23:24:34 +0300

    version 1

---
+++ src/main/java/Intervals.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Yegor on 6/28/2015.
+ */
+public class Intervals {
+    public static void main(String[] args) {
+        log("Intervals started");
+        log("Intervals completed");
+    }
+
+    private static void log(String s){
+        System.out.println(s);
+    }
+
+}
---
+++ src/main/java/Test.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Yegor on 6/28/2015.
+ */
+public class Test {
+
+}
