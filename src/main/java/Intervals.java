f57d7f379c56909f2a78fa20301589780c68d65a
Osi2 <Yegor.Osipov@gmail.com>
2015-06-28 23:32:01 +0300

    version 2

--- src/main/java/Intervals.java
+++ src/main/java/Intervals.java
@@ -4,11 +4,15 @@
 public class Intervals {
     public static void main(String[] args) {
         log("Intervals started");
+        long x = Multiply(100,22);
+        log("multiply: " + String.valueOf(x));
         log("Intervals completed");
     }
 
     private static void log(String s){
         System.out.println(s);
     }
+
+    private static long Multiply(int a, int b){return a*b;}
 
 }
